4eda661ed2e456f36b6f5dce113095db6acdce0d
Kamal Preetam <preetamkamal@gmail.com>
2020-06-21 21:18:57 +0530

    Added server side code with express, cors and pg

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+//middleware
+app.use(cors);
+
+app.listen(5000, () => {
+  console.log("Connected to port 5000");
+});
